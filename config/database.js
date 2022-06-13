9e264ced2ccdf47db8cdef7713f8aca152b0320d
indy <maharanindya2@gmail.com>
2022-06-14 02:31:20 +0700

    done

---
+++ config/database.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const {
+  DB_USER = process.env.DB_USER,
+  DB_PASSWORD = process.env.DB_PASSWORD,
+  DB_NAME = process.env.DB_NAME,
+  DB_HOST = process.env.DB_HOST,
+  DB_PORT = process.env.DB_PORT,
+} = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  test: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}_test`,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres"
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+}
